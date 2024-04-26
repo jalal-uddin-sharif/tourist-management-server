e972f2e31fa9413659e22f57e4ede2bae5cafe63
HyCute <hycute.ts@gmail.com>
2024-04-27 00:43:23 +0600

    make post api and its work

--- index.js
+++ index.js
@@ -26,12 +26,27 @@ async function run() {
     try {
       // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
+        const touristUserCollection = client.db('tourisDB').collection('tourist')
+
+      app.post("/spot-data", async(req, res)=>{
+        const spotData = req.body;
+        console.log(spotData);
+        const result = await touristUserCollection.insertOne(spotData);
+        res.send(result)
+      })
+
+
+
+
+
+
+
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
       // Ensures that the client will close when you finish/error
-      await client.close();
+    //   await client.close();
     }
   }
   run().catch(console.dir);
