23a046839d874c92b833f9d6f4e638cb955bf21f
HyCute <hycute.ts@gmail.com>
2024-04-27 14:06:52 +0600

    server deploy to vercel and pass the test

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const touristUserCollection = client.db("tourisDB").collection("tourist");
 
     app.post("/spot-data", async (req, res) => {
@@ -40,11 +40,13 @@ async function run() {
       res.send(result);
     });
 
+    
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     //   await client.close();
